9d9dce0c619f545dfbd82ccac3712d889ab3575f
Gerard <gerardgisbertvidal@gmail.com>
2026-03-29 16:03:29 +0200

    Extract method amountFor to isolate play pricing logic

---
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -0,0 +1,55 @@
+package theatricalplays;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Map;
+
+public class StatementPrinter {
+
+    public String print(Invoice invoice, Map<String, Play> plays) {
+        var totalAmount = 0;
+        var volumeCredits = 0;
+        var result = String.format("Statement for %s\n", invoice.customer);
+
+        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+
+        for (var perf : invoice.performances) {
+            var play = plays.get(perf.playID);
+            var thisAmount = amountFor(perf, play);
+
+            volumeCredits += Math.max(perf.audience - 30, 0);
+            if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+
+            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+            totalAmount += thisAmount;
+        }
+        result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
+        result += String.format("You earned %s credits\n", volumeCredits);
+        return result;
+    }
+
+    private int amountFor(Performance perf, Play play) {
+        var thisAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error("unknown type: ${play.type}");
+        }
+
+        return thisAmount;
+    }
+}
+
